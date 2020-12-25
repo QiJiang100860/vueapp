5477432f94cc0e3a7ca796591acca7c2e43f1c95
e <jiangqisy@chinamobile.com>
2020-12-25 17:35:02 +0800

    feat: login

--- devApp/src/views/login/index.vue
+++ devApp/src/views/login/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <van-button plain hairline type="info" @click="login">登录</van-button>
-    <van-button plain hairline type="info" @click="loginWebview"
-      >登录webView</van-button
-    >
+    <van-notice-bar
+      left-icon="volume-o"
+      text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。"
+    />
+    <van-grid :column-num="3">
+      <van-grid-item icon="photo-o" text="登录" @click="login" />
+      <van-grid-item icon="photo-o" text="小游戏" @click="game" />
+      <van-grid-item icon="photo-o" text="签名木板" @click="sign" />
+    </van-grid>
+
+    <!-- <van-button plain hairline type="info" @click="login">登录</van-button> -->
   </div>
 </template>
 
@@ -13,13 +20,8 @@ export default {
     login() {
       this.$router.push("home");
     },
-    loginWebview() {
-      const url = window.location.origin + "/home";
-      alert(url);
-      this.$Plus(() => {
-        window.plus.webview.create(url, 1, {}, "");
-      });
-    },
+    game() {},
+    sign() {},
   },
 };
 </script>
