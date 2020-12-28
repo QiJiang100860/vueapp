af3c970c83392153ce9e0f9a7414755dcac16f56
e <jiangqisy@chinamobile.com>
2020-12-28 17:47:50 +0800

    feat: 添加登陆插件

--- devApp/src/components/JInput/index.vue
+++ devApp/src/components/JInput/index.vue
@@ -1,5 +1,84 @@
 <template>
-    <div class="j-input">
-        <input />
+  <div class="j-input">
+    <div class="input-wrapper">
+      <input :value="value" :type="type" @input="change" />
+      <label class="placeholder" :class="value ? 'active' : ''">{{
+        placeholder
+      }}</label>
     </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: "用户名",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+  data(){
+      return {
+          inputVal:""
+      }
+  },
+  methods: {
+    change(e) {
+      const {value} = e.target
+      setTimeout(()=>{
+          this.$emit('input',value)
+      })
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("@/style/theme.less");
+.j-input {
+  padding: 20px 0;
+  .input-wrapper {
+    position: relative;
+    input {
+      display: block;
+      width: 100%;
+      height: 42px;
+      border-bottom: solid 1px #dedede;
+      position: relative;
+      transition: all 0.3s;
+      &:focus {
+        border-bottom: solid 1px @theme-main-color;
+        & + label {
+          left: 0;
+          top: -7px;
+          font-size: 12px;
+          color: #333;
+        }
+      }
+    }
+    label {
+      position: absolute;
+      left: 10px;
+      top: 50%;
+      transform: translateY(-12px);
+      color: #999;
+      transition: all 0.3s;
+      z-index: -1;
+      &.active {
+        left: 0;
+        top: -7px;
+        font-size: 12px;
+        color: #333;
+      }
+    }
+  }
+}
+</style>
--- devApp/src/plugs/Login/index.vue
+++ devApp/src/plugs/Login/index.vue
@@ -2,28 +2,43 @@
   <div v-if="show">
     <van-action-sheet v-model="show" title="登陆">
       <div class="content">
-          <j-input></j-input>
+        <j-input placeholder="请输入用户名" v-model="userName"></j-input>
+        <j-input
+          placeholder="请输入密码"
+          type="password"
+          v-model="password"
+        ></j-input>
+        <van-button type="primary" block @click="login">登陆</van-button>
       </div>
     </van-action-sheet>
   </div>
 </template>
 
 <script>
-import JInput from '@/components/JInput'
+import JInput from "@/components/JInput";
 export default {
-    components:{
-        JInput
-    },
+  components: {
+    JInput,
+  },
   data() {
     return {
       show: true,
+      userName: "",
+      password: "",
     };
+  },
+  methods: {
+    login() {
+      alert("登陆");
+      this.show = !this.show;
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
 .content {
-  height:450px;
+  padding: 0 15px;
+  height: 450px;
 }
 </style>
